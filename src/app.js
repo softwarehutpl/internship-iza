a08c78ff706e062ce2e68c65356b26284b8c405e
izus001 <izabela.baczewska@gmail.com>
2017-07-24 14:27:04 +0200

    carousel fixed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'html-loader?interpolate!./index.html';
 import './less/app.scss';
 
 import 'jquery';
+
 import 'bootstrap';
 
 import './js/pauseCarousel';
--- src/js/pauseCarousel.js
+++ src/js/pauseCarousel.js
@@ -1,3 +1,21 @@
-$('#btn-onoff').on('click', function() { 
-    $('#carouselExampleIndicators').carousel('pause');
+/* $(function () {
+    $('#playButton').click(function () {
+        $('#carouselExampleIndicators').carousel('cycle');
+    });
+    $('#playButton').click(function () {
+        $('#carouselExampleIndicators').carousel('pause');
+    });
+});
+*/
+var flag = 0;
+var indicators = '#carouselExampleIndicators';
+$('#btn-onoff').on('click', function() {
+
+    if(flag == 0) {
+        $(indicators).carousel('pause');
+        flag++;
+    } else {
+        $(indicators).carousel('cycle');
+        flag--;
+    }
 });
